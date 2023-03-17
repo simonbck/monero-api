f660370e22710316cde0ec8d6aeb4f94051bf4e8
simonbck <simonbck@web.de>
2023-03-17 13:45:37 +0100

    added auth

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,18 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        hmr: {
+            protocol: 'ws',
+            https: false,
+            host: '192.168.56.56',
+        },
+        host: "192.168.56.56",
+        https: false,
+        watch: {
+            usePolling: true,
+        },
+    },
     plugins: [
         laravel({
             input: [

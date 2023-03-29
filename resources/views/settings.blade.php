ee0ba2265b538e455616678353bae38817840f66
simonbck <simonbck@web.de>
2023-03-30 01:42:20 +0200

    added form to change the password in the settings page

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -2,6 +2,33 @@
 
 @section('content')
     <div class="container">
-        <h1>Settings</h1>
+        <div class="col-lg-3">
+        <div class="card">
+            <div class="card-header bg-danger text-center">
+                <b>Change password</b>
+            </div>
+            <div class="card-body">
+                <div class="col-lg-12">
+                <form method="POST">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="oldPassword" class="form-label">Old password</label>
+                        <input type="password" class="form-control" id="oldPassword" name="oldPassword">
+                    </div>
+                    <hr>
+                    <div class="mb-3">
+                        <label for="newPassword" class="form-label">New password</label>
+                        <input type="password" class="form-control" id="newPassword" name="newPassword">
+                    </div>
+                    <div class="mb-3">
+                        <label for="ConfirmNewPassword" class="form-label">Confirmation of the new password</label>
+                        <input type="password" class="form-control" id="ConfirmNewPassword" name="ConfirmNewPassword">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Save</button>
+                </form>
+                </div>
+            </div>
+        </div>
+        </div>
     </div>
 @endsection
